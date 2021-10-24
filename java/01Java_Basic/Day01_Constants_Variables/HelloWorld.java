f6de81fdc8a916fbc0c6625104689799d7df54fc
WenqingZhang1999 <74153505+WenqingZhang1999@users.noreply.github.com>
2021-10-24 15:14:03 -0700

    Add files via upload

---
+++ java/01Java_Basic/Day01_Constants_Variables/HelloWorld.java
@@ -0,0 +1,15 @@
+
+//My first programme: HelloWorld
+
+/**
+ * author: Wenqing Zhang
+ */
+
+ public class HelloWorld {
+     public static void main(String[] args) {
+     System.out.println("Hello My programming World");
+         
+     }
+
+}
+
