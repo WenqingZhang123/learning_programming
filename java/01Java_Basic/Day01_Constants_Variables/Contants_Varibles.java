754496eeb3e973ecd7aa5a1880f7bf6333f922ff
WenqingZhang1999 <74153505+WenqingZhang1999@users.noreply.github.com>
2021-10-25 06:47:35 -0700

    Add files via upload

---
+++ java/01Java_Basic/Day01_Constants_Variables/Contants_Varibles.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ * @author zhangwenqing
+ *
+ */
+public class Contants_Varibles {
+	public static void main(String [] args) {
+		//String
+		System.out.println("ABC");
+		System.out.println("");
+		System.out.println("XYZ");
+		
+		// int
+		System.out.println(30);
+		System.out.println(-500);
+		
+		// float
+		System.out.println(3.14);
+		System.out.println(-2.5);
+		
+		// char
+		System.out.println('A');
+		System.out.println('6');
+		
+		// boolean
+		System.out.println(true);
+		System.out.println(false);
+		
+		System.out.println("================");
+		
+		int num1;
+		
+		num1 = 10;
+		System.out.println(num1); // 10
+		
+		num1 = 20;
+		System.out.println(num1); // 20
+		
+
+		int num2 = 25;
+		System.out.println(num2); // 25
+		
+		num2 = 35;
+		System.out.println(num2); // 35
+		System.out.println("===============");
+		
+		byte num3 = 30; 
+		System.out.println(num3); // 30
+		
+		// byte num4 = 400; // overflow, error!
+		
+		short num5 = 50;
+		System.out.println(num5); // 50
+		
+		long num6 = 3000000000L;
+		System.out.println(num6); // 3000000000
+		
+		float num7 = 2.5F;
+		System.out.println(num7); // 2.5
+		
+		double num8 = 1.2;
+		System.out.println(num8); // 1.2
+		
+		char zifu1 = 'A';
+		System.out.println(zifu1); // A
+		
+		zifu1 = '中';
+		System.out.println(zifu1); // 中
+		
+		boolean var1 = true;
+		System.out.println(var1); // true
+		
+		var1 = false;
+		System.out.println(var1); // false
+		
+		
+		boolean var2 = var1;
+		System.out.println(var2); // false
+	}
+}
